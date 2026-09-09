f1b9b783bd548c2f3ac692701c2db8bd4cba10f8
Студент <student@BT.local>
2026-09-09 12:53:39 +0300

    Поправлен отчет, начал выполнение задания

--- tasks/task4/task4/Source.cpp
+++ tasks/task4/task4/Source.cpp
@@ -7,10 +7,17 @@ void main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	srand(time(NULL));
-	int a[10][10], sum_column = 0, sum_string = 0, answer;
-	for (int i = 0; i < 10; i++) {
+	int sum_column = 0, sum_string = 0, answer, size;
+	int* rows;
+	int* string;
+	printf("Введите размер квадратной матрицы: %d");
+	scanf("%d", &size);
+	rows = (int*)calloc(size * sizeof(int));
+	string = (int*)malloc(size * sizeof(int));
+	
+	for (; < 10; i++) {
 		for (int j = 0; j < 10; j++) {
-			a[i][j] = rand()%10;
+			a[i][j] = rand()%50-29;
 		}
 	}
 	for (int i = 0; i < 10; i++) {
@@ -21,22 +28,11 @@ void main() {
 	}
 	printf("Какую сумму вы хотите найти? (1 - суммы строк, 2 - суммы столбцов): ");
 	scanf_s("%d", &answer);
-	if (answer == 1) {
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				sum_string += a[i][j];
-			}
-			printf("Сумма строки %d: %d\n", i, sum_string);
-			sum_string = 0;
-		}
-	}
-	else {
+	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
-			for (int i = 0; i < 10; i++) {
-				sum_column += a[i][j];
-			}
-			printf("Сумма столбца %d: %d\n", j, sum_column);
-			sum_column = 0;
+			sum_string += a[i][j+1];
 		}
+		printf("Сумма строки %d: %d\n", i, sum_string);
+		sum_string = 0;
 	}
 }
